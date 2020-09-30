388a236e9e50431919fc975181b09a4f67b2cc01
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2020-09-30 11:25:39 -0300

    Solved the background color by scrolling the scroll bar

--- src/styles/global.js
+++ src/styles/global.js
@@ -20,6 +20,7 @@ export default createGlobalStyle`
 
 html, body, #root {
   height: 100%;
+  background: linear-gradient(-90deg, #3e3e3e, #595959);
 
   body {
     -webkit-font-smoothing: antialiased;
